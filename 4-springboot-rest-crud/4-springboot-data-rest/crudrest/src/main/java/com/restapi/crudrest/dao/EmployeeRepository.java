a0c72aba90b2ac2ddd717200ea8e4c02d4064d8d
ransher <ransherraj.com>
2024-03-08 17:17:35 +0530

    slight update

--- 4-springboot-rest-crud/4-springboot-data-rest/crudrest/src/main/java/com/restapi/crudrest/dao/EmployeeRepository.java
+++ 4-springboot-rest-crud/4-springboot-data-rest/crudrest/src/main/java/com/restapi/crudrest/dao/EmployeeRepository.java
@@ -2,7 +2,10 @@ package com.restapi.crudrest.dao;
 
 import com.restapi.crudrest.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource(path = "members")
+// api path + "/members"
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     //that is it
